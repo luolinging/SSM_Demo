60be7cc5463a80ac8146e7dc3829ebfb013550e8
luoll <1968967624@qq.com>
2020-09-24 12:15:56 +0800

    解决bean冲突的问题

--- src/main/java/com/lll/student/service/impl/StudentServiceImpl.java
+++ src/main/java/com/lll/student/service/impl/StudentServiceImpl.java
@@ -3,7 +3,6 @@ package com.lll.student.service.impl;
 import com.lll.student.common.MyException;
 import com.lll.student.dao.IStudentDao;
 import com.lll.student.domain.Student;
-import com.lll.student.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +12,12 @@ import java.util.List;
  * 业务层的实现类
  * @author luo
  */
-@Service("studentService")
-public class StudentServiceImpl implements IStudentService {
+@Service
+public class StudentServiceImpl{
 
     @Autowired
     private IStudentDao studentDao;
 
-    @Override
     public void insert(Student student) {
         if (student.getGrade_id() > 100){
             throw new MyException("自定义异常");
@@ -27,28 +25,23 @@ public class StudentServiceImpl implements IStudentService {
         studentDao.insert(student);
     }
 
-    @Override
     public void delete(Long id) {
         studentDao.delete(id);
     }
 
-    @Override
     public void update(Student student) {
         studentDao.update(student);
     }
 
-    @Override
     public List<Student> findAll() {
         return studentDao.findAll();
     }
 
-    @Override
     public Student findById(Long id) {
-        int i = 1/0;
+        /*int i = 1/0;*/
         return studentDao.findById(id);
     }
 
-    @Override
     public List<Student> findByName(String name) {
         return studentDao.findByName(name);
     }
--- src/test/java/com/lll/student/TestSpring.java
+++ src/test/java/com/lll/student/TestSpring.java
@@ -1,6 +1,5 @@
 package com.lll.student;
 
-import com.lll.student.service.IStudentService;
 import com.lll.student.service.impl.StudentServiceImpl;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -10,7 +9,7 @@ public class TestSpring {
     @Test //测试spring的环境是否正常
     public void test(){
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
-        IStudentService studentService = applicationContext.getBean("studentService", StudentServiceImpl.class);
+        StudentServiceImpl studentService = applicationContext.getBean("studentService", StudentServiceImpl.class);
         studentService.findAll();
     }
 }
